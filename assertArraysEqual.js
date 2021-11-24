6df2bbfc09cb1ce73f6f1e55bf76232ebd5adc20
Desmond Parker <evan.parker95@gmail.com>
2021-11-24 00:44:08 +0000

    adding assertArraysEqual.js to the lotide project, w1d2

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const eqArrays = function (one, two) {
+  if (Array.isArray(one) && Array.isArray(two)) {
+    if (one.length === two.length) {
+      let check = true
+      for (let i = 0; i < one.length; i++) {
+        if (one[i] !== two[i]) {
+          return false
+        }
+      }
+      return true
+    }
+  }
+  else {
+    return false
+  }
+}
+
+const assertArraysEqual = function (uno, dos) {
+  if (eqArrays(uno, dos) === true) {
+    console.log("👌 Wow, it's looking clean, they're the same! 👍")
+  } else {
+    console.log("😱 Uh, oh! Something goofed! 😥")
+  }
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+
+
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
