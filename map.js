10f9dda36ee56132e91413ab7cd3ca58a78ceb10
Desmond Parker <evan.parker95@gmail.com>
2021-11-25 22:33:57 +0000

    adding map.js to lotide project

---
+++ map.js
@@ -0,0 +1,39 @@
+const eqArrays = function (one, two) {
+  if (Array.isArray(one) && Array.isArray(two)) {
+    if (one.length === two.length) {
+      for (let i = 0; i < one.length; i++) {
+        if (one[i] !== two[i]) {
+          return false
+        }
+      }
+      return true
+    }
+  }
+  else {
+    return false
+  }
+}
+
+const assertArraysEqual = function (uno, dos) {
+  if (eqArrays(uno, dos) === true) {
+    console.log("👌 Wow, it's looking clean, they're the same! 👍")
+  } else {
+    console.log("😱 Uh, oh! Something goofed! 😥")
+  }
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+const words2 = ["gary", "conner", "tom", "michael", "tony"]
+
+const map = function (array, callback) {
+  const results = []
+  for (let item of array) {
+    results.push(callback(item))
+  }
+  return results
+}
+
+//const results1 = map(words, word => word[0]);
+//console.log(results1);
+
+//assertArraysEqual(map(words, word => word[0]), map(words2, word => word[0]))
