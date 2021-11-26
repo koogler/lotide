0e060a8adbc2e86a0dd490c54f3f179561870618
Desmond Parker <evan.parker95@gmail.com>
2021-11-26 03:03:05 +0000

    adding findKey.js to lotide project

---
+++ findKey.js
@@ -0,0 +1,37 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😁😁😁Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😱😱😱Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+  let objKey = Object.keys(object)
+  for (const key of objKey) {
+    if (callback(object[key])) {
+      return key
+    }
+  }
+}
+
+function x(value) {
+}
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2)
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma")
