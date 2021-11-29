a91f7da9b455291be80f95f949d7b550201c99c2
Desmond Parker <evan.parker95@gmail.com>
2021-11-29 19:56:53 +0000

    refactoring headTest.js to use mocha and chai to test instead of regular console.log testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,15 @@
-const head = require('../head.js');
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head = require('../head');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 4);
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "nothing");
-assertEqual(head([5]), 5);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  })
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 'hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
